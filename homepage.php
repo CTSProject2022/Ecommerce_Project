b7d652efb8dbfe099ae141a6b66dcc5a4480e872
Ray Cummings <ray.cummings40@yahoo.com>
2021-10-27 20:24:37 -0400

    Notify User login on home page

--- homepage.php
+++ homepage.php
@@ -5,7 +5,7 @@ if (isset($_SESSION["user_id"])) {
     $first_name = $_SESSION["first_name"];
     $last_name = $_SESSION["last_name"];
 
-    echo "Welcome $first_namr $last_name";
+    echo "Welcome $first_name $last_name";
 }
 
 
