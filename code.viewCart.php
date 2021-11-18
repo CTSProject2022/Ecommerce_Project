f9cd1212e48f59347ef38542b1f1543f97cbed62
Ray Cummings <ray.cummings40@yahoo.com>
2021-11-17 21:22:59 -0400

    Edited code.viewCart.php

--- code.viewCart.php
+++ code.viewCart.php
@@ -1,16 +1,16 @@
 <?php //PHP header
 
 
-
-
 include "connection.php";
 
-$sql = "SELECT * FROM SELECT * FROM cart, items WHERE cart.item_id = items.item_id AND cart.user_id = 1 AND cart.cart_item_status = 'cart' ";
+$sql = "SELECT * FROM  cart, items WHERE cart.item_id = items.item_id AND cart.user_id = $user_id AND cart.cart_item_status = 'cart' ";
+
+
+
 $result = mysqli_query($conn, $sql);
 
-
-
 if (mysqli_num_rows($result) > 0) {
+
 
     $cart = mysqli_fetch_all($result, MYSQLI_ASSOC);
     foreach ($cart as $item) {
